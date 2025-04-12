893d5f15d416705da2099a50c45df6909883e3bb
Pierrick Labrot <pierrick.s.labrot@gmail.com>
2025-04-12 12:23:30 -0400

    modelfinish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Movie.create(title: 'Interstellar',
+  overview: 'a group of astronauts who travel through a wormhole near Saturn in search of a new home for mankind.',
+  poster_url: 'https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQpJ5xLbWZVR80gVGpUwu-nt6pLlZK-clSi2ng0S58kVjH9EZ1PLO-QicqYKn5kCH8TpAzViZlIojuk1048',
+  rating: 9.1
+)
